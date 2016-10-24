03d758435492d400a21a4aec65c1e046955d0594
kiss <zpssssn@gmail.com>
2016-10-24 09:26:30 +0800

    add getting ip

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+#secondupload
+新增本地IP地址获取，并打印，方便查看
+var os = require('os');
+var IPv4,hostName;
+hostName=os.hostname();
+for(var i=0;i<os.networkInterfaces().en0.length;i++){
+    if(os.networkInterfaces().en0[i].family=='IPv4'){
+        IPv4=os.networkInterfaces().en0[i].address;
+    }
+}  
 #FirstUpload
 this js port is 2000,you can make download server
 # nodejsDownloadServer
--- downloadserver.js
+++ downloadserver.js
@@ -1,7 +1,16 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var os = require('os');
+var IPv4,hostName;
+hostName=os.hostname();
+for(var i=0;i<os.networkInterfaces().en0.length;i++){
+    if(os.networkInterfaces().en0[i].family=='IPv4'){
+        IPv4=os.networkInterfaces().en0[i].address;
+    }
+}  
 app.use("/",express.static(__dirname + '/') );
 http.createServer(app).listen('2000',function(){
-  console.log("start ok,port:2000");
+  console.log(IPv4 + ":2000");
+  console.log('----------local host: '+hostName);
 })
