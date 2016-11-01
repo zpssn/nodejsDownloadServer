d94a6b9742f05c9d118b56b6f46d01680a845f17
zpssn <920167280@qq.com>
2016-11-01 15:50:48 +0800

    Update downloadserver.js
    添加 en0说明

--- downloadserver.js
+++ downloadserver.js
@@ -4,7 +4,7 @@ var app = express();
 var os = require('os');
 var IPv4,hostName;
 hostName=os.hostname();
-for(var i=0;i<os.networkInterfaces().en0.length;i++){
+for(var i=0;i<os.networkInterfaces().en0.length;i++){     //en0 is your Network Interface Card, be careful ,use ifconfig to see what is your Network Interface Card and change
     if(os.networkInterfaces().en0[i].family=='IPv4'){
         IPv4=os.networkInterfaces().en0[i].address;
     }
