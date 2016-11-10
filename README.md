3aff8e504c669e753c3c46b3dd6cff228fedb1a3
kiss <kiss@pengdeMacBook-Pro.local>
2016-11-10 15:53:52 +0800

    thirdupdate

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+#thirdupload
+新增router文件夹，内含index.js
+res.download('Desktop/nodejsDownloadServer/yourfilename.extension','yourfilename.extension');//某路径下文件名字，返回到客户端具体的文件名字
+只要用户请求“/”空白路径即可返回规定路径下需要下载的文件
 #secondupload
 新增本地IP地址获取，并打印，方便查看
 var os = require('os');
--- downloadserver.js
+++ downloadserver.js
@@ -1,15 +1,17 @@
 var http = require('http');
 var express = require('express');
+var routes = require('./routes/index');
 var app = express();
 var os = require('os');
 var IPv4,hostName;
 hostName=os.hostname();
-for(var i=0;i<os.networkInterfaces().en0.length;i++){     //en0 is your Network Interface Card, be careful ,use ifconfig to see what is your Network Interface Card and change
+for(var i=0;i<os.networkInterfaces().en0.length;i++){
     if(os.networkInterfaces().en0[i].family=='IPv4'){
         IPv4=os.networkInterfaces().en0[i].address;
     }
-}  
-app.use("/",express.static(__dirname + '/') );
+}
+app.use('/', routes);
+//app.use("/",express.static(__dirname + '/') );
 http.createServer(app).listen('2000',function(){
   console.log(IPv4 + ":2000");
   console.log('----------local host: '+hostName);
