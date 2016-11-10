918ff98e5df07054ed2caeed64f349ab554c9110
kiss <kiss@pengdeMacBook-Pro.local>
2016-11-10 16:19:21 +0800

    fithupload

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res, next) {
-  res.download('Desktop/nodejsDownloadServer/yourfilename.extension','yourfilename.extension');
+  res.download('Desktop/nodejsDownloadServer/README.md','README.md');
+  console.log(“download——》》》》》》》success”);
 });
 
 module.exports = router;
