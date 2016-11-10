8b18b2ba0a6f30417615f998485f24c431934f6a
kiss <kiss@pengdeMacBook-Pro.local>
2016-11-10 15:57:53 +0800

    forthupload

--- downloadserver.js
+++ downloadserver.js
@@ -5,7 +5,7 @@ var app = express();
 var os = require('os');
 var IPv4,hostName;
 hostName=os.hostname();
-for(var i=0;i<os.networkInterfaces().en0.length;i++){
+for(var i=0;i<os.networkInterfaces().en0.length;i++){   //en0 is your Network Interface Card, be careful ,use ifconfig to see what is your Network Interface Card and change
     if(os.networkInterfaces().en0[i].family=='IPv4'){
         IPv4=os.networkInterfaces().en0[i].address;
     }
@@ -16,3 +16,33 @@ http.createServer(app).listen('2000',function(){
   console.log(IPv4 + ":2000");
   console.log('----------local host: '+hostName);
 })
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
